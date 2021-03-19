bdbc7ad16fdf31745519d9f9e299dc8d77c9723a
jhonny606 <79945241+jhonny606@users.noreply.github.com>
2021-03-19 17:30:19 -0300

    Estudo Dirigido

---
+++ EstudoDirigido/src/animal/Mosca.java
@@ -0,0 +1,13 @@
+package animal;
+
+public class Mosca{
+    private int qtdPatas;
+
+    public int getQtdPatas() {
+        return qtdPatas;
+    }
+
+    public void setQtdPatas(int qtdPatas) {
+        this.qtdPatas = qtdPatas;
+    }
+}
