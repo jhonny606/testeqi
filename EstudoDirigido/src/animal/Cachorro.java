8aeaa0ee660a0fcead1270d95829d516dbb45a31
jhonny606 <79945241+jhonny606@users.noreply.github.com>
2021-03-19 17:26:10 -0300

    Estudo Dirigido

---
+++ EstudoDirigido/src/animal/Cachorro.java
@@ -0,0 +1,13 @@
+package animal;
+
+public class Cachorro{
+    private String corPelo;
+
+    public String getCorPelo() {
+        return corPelo;
+    }
+
+    public void setCorPelo(String corPelo) {
+        this.corPelo = corPelo;
+    }
+}
---
+++ EstudoDirigido/src/animal/Papagaio.java
@@ -0,0 +1,13 @@
+package animal;
+
+public class Papagaio{
+    private String corPena;
+
+    public String getCorPena() {
+        return corPena;
+    }
+
+    public void setCorPena(String corPena) {
+        this.corPena = corPena;
+    }
+}
